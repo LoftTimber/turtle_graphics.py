93f0f37705af13ffa8862be749370686de3c285c
LoftTimber <31486781+LoftTimber@users.noreply.github.com>
2017-10-25 14:51:28 -0400

    Update turtle_graphics.py

--- turtle_graphics.py
+++ turtle_graphics.py
@@ -1,28 +1,45 @@
 from turtle import *
 pen1=Pen()
 pen2=Pen()
+pen3=Pen()
 
 setworldcoordinates(-1000,-1000,1000,1000)
 screensize(10,10,'teal')
 def star(x,y):
     speed(99)
+    points=1
     
-    goto(x,y=None)
+    goto(x,y)
+    pendown()
     color('red', 'yellow')
     begin_fill()
-    for i in range(0,36):
+    for i in range(0,points):
         forward(200)
-        left(170)
+        left(360/points)
         
     end_fill()
-    done()
-
-
-
-
-
-onscreenclick(star(x,y))
-    
+    penup()
     
 
+x=xcor()
+y=ycor()
 
+def dr_p2():
+    pen2.speed(0)
+    pen2.goto(0,-1000)
+    pen2.forward(1000)
+    for i in range(0,4):
+        forward(200)
+        left(90)
+
+def dr_p3():
+    pen3.speed(0)
+    pen3.goto(0,-1000)
+    pen3.back(1000)
+    
+
+dr_p2()
+dr_p3()
+
+penup()
+onscreenclick(star)
