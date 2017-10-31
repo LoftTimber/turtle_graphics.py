b86796438e059f36327787ce715425958c8446c1
LoftTimber <31486781+LoftTimber@users.noreply.github.com>
2017-10-31 00:39:38 -0400

    Update turtle_graphics.py

--- turtle_graphics.py
+++ turtle_graphics.py
@@ -1,63 +1,9 @@
 from turtle import *
-
+import time
 
 pen2=Pen()
-pen3=Turtle()
 
-setworldcoordinates(-1000,-1000,1000,1000)
-screensize(10,10,'skyblue')
-def plant(x,y):
-
-    
-
-    
-    speed(0)
-    points=36
-    penup()
-    
-    goto(x,y)
-    
-    
-    #leafs
-    color('green','green')
-    width(1)
-    
-    
-    goto(x,y)
-    begin_fill()
-    for i in range(0,2):
-        left(135)
-        forward(100)
-        left(45)
-        forward(100)
-    for i in range(0,2):
-        forward(100)
-        left(45)
-        forward(100)
-        left(135)
-    end_fill()
-    left(90)
-    penup()
-     #stem
-    color('lightgreen')
-    
-    width(10)
-    pendown()
-    goto(x,-900)
-    forward(11.5)
-    end_fill()
-    penup()
-    
-    
-    
-    
-    
-    
-    ht()
-    
-
-x=xcor()
-y=ycor()
+'''
 
 def dr_p2():
     pen2.color('darkgreen','darkgreen')
@@ -80,36 +26,96 @@ def dr_p2():
 
 
 
-def rose(x,y):
+'''
+
+setworldcoordinates(-1000,-1000,1000,1000)
+screensize(10,10,'skyblue')
+def plant(x,y):
+       
+    speed(0)
+    penup()
+    goto(x,y)
+    
+    #leafs
+    color('green','green')
+    width(1)
+    goto(x,y)
+    begin_fill()
+    left(135)
+    for i in range(0,2):
+        right(135)
+        for i in range(0,2):
+            left(135)
+            forward(100)
+            left(45)
+            forward(100)
+    end_fill()
+    left(225)
+    penup()
+
+    #stem
+    color('lightgreen')
+    width(10)
+    pendown()
+    goto(x,-900)
+    forward(11.5)
+    end_fill()
+    penup()
+    
+    #hideturtle
+    ht()
+    
+
+x=xcor()
+y=ycor()
+
+def dr_p2():
+    pen2.color('darkgreen','darkgreen')
+    pen2.speed(0)
+    pen2.penup()
+    pen2.goto(-10000,-900)
+    pen2.pendown()
+    pen2.begin_fill()
+    s=0
+    while s<2:  
+        pen2.forward(20000)
+        pen2.right(90)
+        pen2.forward(1000)
+        pen2.right(90)
+        s+=1
+    pen2.end_fill()
+    pen2.ht()
+
+        
+
+
+
+def rose(x,y,r_1,r_2):
     speed(0)
     penup()
     left(0)
     goto(x,y)
     width(1)
-    color('black','red')
+    color(r_1,r_2)
     pendown()
     left(52.5)
     begin_fill()
     for i in range(0,3):
         right(15)
-        for i in range(0,2):
-            
-             
-            
+        for i in range(0,2):           
             forward(150)
             right(45)
             forward(150)
-            right(135)
-            
+            right(135)       
     end_fill()
     right(97.5)
     ht()
+
+
+    
 def sunflower(x,y):
     penup()
-    right(90)
-    
     goto(x,y)
-    left(90)
     forward(150)
     color('brown','yellow')
     width(1)
@@ -140,30 +146,39 @@ def sunflower(x,y):
 
 
 
-
-
-
 dr_p2()
+
+
+
 def pick_s(x,y):
-        
     
-        
     plant(x,y)
     sunflower(x,y)
         
-    
-    
-    
+
 
 def pick_r(x,y):
+    if toggle:
+        r_1='black'
+        r_2='white'
+    elif not toggle:
+        r_1='black'
+        r_2='red'
     plant(x,y)
-    rose(x,y)
+    rose(x,y,r_1,r_2)
+
+toggle=True
+def tgge():
+    if toggle:
+        toggle= not toggle
+    elif not toggle:
+        toggle= toggle
 
 
+onkey(tgge,"space")
 
 onscreenclick(pick_s,btn=3)
 onscreenclick(pick_r,btn=1)
-
 
 
 
